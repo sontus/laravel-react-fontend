4f01f1696cd9bea171d38515bb0b183ff2f4268a
Sontus Chandra Anik <info.sontus@gmail.com>
2023-01-21 16:48:53 +0600

    All Books Fatch

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 import React, { useState,useEffect } from 'react'
 
 export const Books = () => {
-    const [booklist, setBooks] = useState('');
+    const [bookList, setBookList] = useState('');
     const url = "http://127.0.0.1:8000/api/book-list";
 
     useEffect(() => {
@@ -16,14 +16,15 @@ export const Books = () => {
         .then((response) => {
             const allBooks = response.data;
             console.log(allBooks);
-            setBooks(allBooks);
+            setBookList(allBooks);
         })
         .catch(error => console.error(`Error : ${error}`));
     }
   return (
     <div>
-    
-       
+        <div>
+
+        </div>
     </div>
   )
 }
