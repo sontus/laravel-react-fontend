c1702c79347145b0e0425c4d0b95902edf73d9c9
Sontus Chandra Anik <info.sontus@gmail.com>
2023-01-21 16:35:00 +0600

    Book List Added

---
+++ src/components/Books/Books.js
@@ -0,0 +1,29 @@
+import Axios from 'axios';
+import React, { useState,useEffect } from 'react'
+
+export const Books = () => {
+    const [booklist, setBooks] = useState('');
+    const url = "http://127.0.0.1:8000/api/book-list";
+
+    useEffect(() => {
+        getAllBooks();
+    },[]);
+
+
+
+    const getAllBooks = () => {
+        Axios.get(url)
+        .then((response) => {
+            const allBooks = response.data;
+            console.log(allBooks);
+            setBooks(allBooks);
+        })
+        .catch(error => console.error(`Error : ${error}`));
+    }
+  return (
+    <div>
+    
+       
+    </div>
+  )
+}
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,20 @@
-import React, { Component } from "react";
+import React, { Component,useState } from "react";
 import { Button } from "reactstrap";
 import { Redirect } from "react-router-dom";
+import "./Home.css";
+import axios from "axios";
+import { Books } from "../Books/Books";
 
 export default class Home extends Component {
+
+ 
+
   state = {
     navigate: false,
   };
+
+  
+
 
   onLogoutHandler = () => {
     localStorage.clear();
@@ -24,8 +33,9 @@ export default class Home extends Component {
         <h3> HomePage</h3>
         <div className="row">
           <div className="col-xl-9 col-sm-12 col-md-9 text-dark">
-            <h5> Welcome, {user.first_name} </h5> You have Logged in
+            <h5> Welcome, to Your Dashboard </h5> You have Logged in
             successfully.
+            <Books></Books>
           </div>
           <div className="col-xl-3 col-sm-12 col-md-3">
             <Button
